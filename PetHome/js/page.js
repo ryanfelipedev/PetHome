dbacc509db7f5f3fe51af04062284f98eab70385
ryanfelipedev <ryaanfeliipe22@gmail.com>
2025-04-14 19:35:33 -0300

    Commit projeto PetShop

---
+++ PetHome/js/page.js
@@ -0,0 +1,13 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.efeito-txt-topo',{
+    duration:3000,
+    distance:'90px'
+})
+
+revelar.reveal('.efeito-img-topo',{
+    duration:3000,
+    distance:'90px',
+    delay: 100
+})
+
